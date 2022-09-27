88edf8a59fba59f1f0c5a072dd2cbff5c6d9f31e
Giorgio Buzzanca <giorgio.buzzanca@studio.unibo.it>
2022-09-27 20:05:24 +0200

    feat(utils): implement improved version of binarization

--- utils/image_processing.py
+++ utils/image_processing.py
@@ -1,11 +1,19 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KernelDensity
+import scipy
+import scipy.signal
 
 
-def binarizeImage(image: np.ndarray) -> np.ndarray:
-    blurred_img = cv.boxFilter(src=image, ddepth=-1, ksize=(3,3))
-    threshold, binarized_image = cv.threshold(blurred_img, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+def binarizeImage(image: np.ndarray, kde_bandwidth: int = 5) -> np.ndarray:
+    X = np.arange(255)[:, None]
+    kde = KernelDensity(kernel='gaussian', bandwidth=kde_bandwidth).fit(image.ravel()[:, None])
+    log_dens = kde.score_samples(X)
+    threshold = scipy.signal.argrelmin(log_dens)[0][0]
+    binarized_image = np.zeros(image.shape, dtype = image.dtype)
+    binarized_image[image < threshold] = 0
+    binarized_image[image >= threshold] = 255
     return threshold, 255 - binarized_image
 
 
@@ -23,7 +31,7 @@ def plot_connected_components(n_labels: int, label_ids: np.ndarray, stats: np.nd
         labeled_img = cv.rectangle(labeled_img,
                                    (stats[cc][0], stats[cc][1]),
                                    (stats[cc][0] + stats[cc][2], stats[cc][1] + stats[cc][3]),
-                                   (0, 0, 255), 1)
+                                   (255, 255, 255), 1)
         labeled_image = cv.circle(labeled_img, (int(centroids[cc][0]), int(centroids[cc][1])), radius=2,
                                   color=(0, 0, 0), thickness=-1)
 
